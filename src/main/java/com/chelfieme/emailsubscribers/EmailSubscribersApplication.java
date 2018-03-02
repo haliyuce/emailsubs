55dd530fbd611a1ed79c1007e1db465697dc4d3f
hasanali.yuce <haliyuce@gmail.com>
2018-03-02 10:06:16 +0300

    fix

--- src/main/java/com/chelfieme/emailsubscribers/EmailSubscribersApplication.java
+++ src/main/java/com/chelfieme/emailsubscribers/EmailSubscribersApplication.java
@@ -2,11 +2,26 @@ package com.chelfieme.emailsubscribers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class EmailSubscribersApplication {
+@Configuration
+public class EmailSubscribersApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailSubscribersApplication.class, args);
+	}
+
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/" };
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
 	}
 }
